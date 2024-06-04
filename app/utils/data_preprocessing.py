6c01205d3bc376d20af646b23b9c13956fbb53eb
LEONARDO_DIAS <leonardodias2706@gmail.com>
2024-06-04 15:43:11 -0300

    handling pipeline improvements

--- app/utils/data_preprocessing.py
+++ app/utils/data_preprocessing.py
@@ -1,19 +1,57 @@
 import pandas as pd
+import joblib
+from sklearn.preprocessing import StandardScaler
 class PreprocPipeline:
     '''
     Custom transformer for data preprocessing
     '''
     def __init__(self):
-        self.drop_columns = ['oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest', 'isFlaggedFraud']
+        # Our training dataset has only numerical features
+        self._scaler = StandardScaler()
+        self.drop_columns = ['nameOrig', 'nameDest', 'isFraud', 'isFlaggedFraud']
+        self.fitted = False
+        #No need for imputers as there are no missing values in the dataset
 
-    def preprocess_data(self, data):
+
+    def fit(self,  X_train):    
         '''
-        Preprocess the input data
+        Fit the transformer on input data
         '''
-        # Drop unnecessary columns
+        X_copy = X_train.drop(columns=self.drop_columns)
+        X_copy = pd.DataFrame(self._scaler.fit_transform(X_train))
+        self.fitted = True
+        return self
+    
+
+    def transform(self, X):
+        '''
+        Preprocess the input data for model training
+        '''
+        if not self.fitted:
+            raise ValueError('Transformer must be fitted before transforming data')
         data = data.drop(columns=self.drop_columns)
-        
-        # One-hot encode the 'type' feature
-        data = pd.get_dummies(data, columns=['type'], drop_first=True)
-        
-        return data
+        X = pd.DataFrame(self._scaler.transform(X))
+        return X
+    
+
+    def encoder(self , y):
+        '''
+        Encode the target variable
+        '''
+        y = pd.get_dummies(y, drop_first=True)
+        return y
+    
+
+    def save_pipeline(self):
+        '''
+        Save the preprocessing pipeline
+        '''
+        joblib.dump(self, 'models/preprocessing_pipeline.pkl')
+
+
+    @staticmethod
+    def load_pipeline():
+        '''
+        Load the preprocessing pipeline
+        '''
+        return joblib.load('models/pipeline/preprocessing_pipeline.pkl')
