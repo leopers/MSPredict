76a7d8dda828d675449841846d3fa401a83ea18e
LEONARDO_DIAS <leonardodias2706@gmail.com>
2024-06-04 14:33:02 -0300

    exploratory data analysis & initial declaration of preproc pipeline

--- app/utils/data_preprocessing.py
+++ app/utils/data_preprocessing.py
@@ -1,10 +1,19 @@
 import pandas as pd
+class PreprocPipeline:
+    '''
+    Custom transformer for data preprocessing
+    '''
+    def __init__(self):
+        self.drop_columns = ['oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest', ' isFlaggedFraud']
 
-def preprocess_data(data):
-    # Drop unnecessary columns
-    data = data.drop(columns=['oldbalanceOrg', 'newbalanceOrig', 'oldbalanceDest', 'newbalanceDest'])
-    
-    # One-hot encode the 'type' feature
-    data = pd.get_dummies(data, columns=['type'], drop_first=True)
-    
-    return data
+    def preprocess_data(self, data):
+        '''
+        Preprocess the input data
+        '''
+        # Drop unnecessary columns
+        data = data.drop(columns=self.drop_columns)
+        
+        # One-hot encode the 'type' feature
+        data = pd.get_dummies(data, columns=['type'], drop_first=True)
+        
+        return data
