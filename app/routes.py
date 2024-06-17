d85022463687d3402e210c615c818aa48d56dde6
jrsHenrique <jrs.henrique9@gmail.com>
2024-06-17 03:32:47 -0300

    all set, waiting to load model and to link online database

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 main_bp = Blueprint('main_bp', __name__)
 
+model = joblib.load('../model/pipeline.pkl')
+
 @main_bp.route('/')
 def home():
     return render_template('home.html')
@@ -34,29 +36,42 @@ def dashboard():
 def services():
     return render_template('services.html')
 
-# @main_bp.route('/about')
-# def load_user_credentials():
-#     csv_path = '../app/data/users.csv'
-#     users_df = pd.read_csv(csv_path)
-#     users_df['password'] = users_df['password'].astype(str)
-#     users = dict(zip(users_df['username'], users_df['password']))
-#     return users
-
 @main_bp.route('/form', methods=['GET', 'POST'])
 def form():
     if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
+        # Retrieve form data
+        column1 = request.form['column1']
+        column2 = request.form['column2']
+        column3 = request.form['column3']
+        
+        # Insert data into PostgreSQL
+        try:
+            params = config()
+            connection = psycopg2.connect(**params)
+            cursor = connection.cursor()
 
-        conn = get_db_connection()
-        cur = conn.cursor()
-        cur.execute('INSERT INTO users (name, email) VALUES (%s, %s)', (name, email))
-        conn.commit()
-        cur.close()
-        conn.close()
+            # Construct the SQL query
+            insert_query = "INSERT INTO my_table (column1, column2, column3) VALUES (%s, %s, %s)"
+            cursor.execute(insert_query, (column1, column2, column3))
+            
+            # Commit the transaction
+            connection.commit()
+            
+            # Close cursor and connection
+            cursor.close()
+            connection.close()
 
-        return redirect(url_for('main_bp.home'))
+            return redirect(url_for('main_bp.success'))
+        
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            return "An error occurred while inserting data into the database."
+    
     return render_template('form.html')
+
+@main_bp.route('/success')
+def success():
+    return "Data successfully inserted into the database!"
 
 @main_bp.route('/upload', methods=['GET', 'POST'])
 def upload():
@@ -83,6 +98,29 @@ def upload():
 
 @main_bp.route('/check_frauds')
 def check_frauds():
+    if request.method == 'POST':
+        try:
+            # Retrieve form data
+            amount = float(request.form['amount'])
+            date = pd.to_datetime(request.form['date'])
+
+            # Prepare data for prediction
+            transaction_data = pd.DataFrame({'amount': [amount], 'date': [date]})
+            
+            # Make prediction using the loaded model
+            prediction = model.predict(transaction_data)[0]
+
+            if prediction == 1:
+                fraud_status = 'Fraudulent'
+            else:
+                fraud_status = 'Not Fraudulent'
+
+            return render_template('fraud_result.html', prediction=fraud_status, amount=amount, date=date)
+        
+        except Exception as e:
+            print(e)
+            return "An error occurred while evaluating fraud."
+
     return render_template('check_frauds.html')
 
 def load_user_credentials():
