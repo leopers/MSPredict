354f996f33692becea3eb12cc9d6b6e251bb1cbd
LEONARDO_DIAS <leonardodias2706@gmail.com>
2024-06-17 00:49:20 -0300

    some testing

--- tests/test_preporocessing.py
+++ tests/test_preporocessing.py
@@ -1,16 +1,37 @@
+from json import load
 from app.utils.data_preprocessing import PreprocPipeline
 import pandas as pd
 import numpy as np
 
 
-data = pd.read_csv('data/processed/processed_train.csv')
+#Testing if loading pipeline works
 
-df = data.copy()
+data = {
+    'transaction_id': [1, 2, 3],
+    'amount(usd)': [100.0, 200.0, 300.0],
+    'lat': [34.05, 36.16, 40.71],
+    'long': [-118.24, -115.15, -74.01],
+    'merch_lat': [34.05, 36.16, 40.71],
+    'merch_long': [-118.24, -115.15, -74.01],
+    'age': [25, 35, 45],
+    'merchant': ['A', 'B', 'A'],
+    'job': ['engineer', 'doctor', 'teacher'],
+    'hour_of_day': [12, 15, 18],
+    'month': [1, 2, 3],
+    'day_of_week': [1, 2, 3]
+}
 
-preprocessor = PreprocPipeline()
+df = pd.DataFrame(data)
 
-X = df.copy()
-X = preprocessor.select_features(X)
+# Instantiate the pipeline
+pipeline = PreprocPipeline()
 
-# save the new dataframe
-X.to_csv('data/test/processed_train_selected.csv', index=False)
+# Fit the pipeline
+pipeline.fit(df)
+
+# Save the pipeline
+pipeline.save_pipeline()
+
+loaded_pipeline = PreprocPipeline.load_pipeline()
+
+loaded_pipeline.transform(df)
